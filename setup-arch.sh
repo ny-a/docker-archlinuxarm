eabd0c6bdf1696dcaa4c6d0a427d427103b9f32d
nyaarch64 <58758248+ny-a@users.noreply.github.com>
2025-09-07 12:42:58 +0900

    Add workaround for pacman makepkg.conf CFLAGS bug

--- setup-arch.sh
+++ setup-arch.sh
@@ -2,13 +2,16 @@
 
 set -exuo pipefail
 
+ARMV7_ARCHITECTURE="armv7"
+
 GPG_KEY_ID="77193F152BDBE6A6"
 MOUNTPOINT="/mnt"
+ARCHITECTURE="$(cat /etc/apk/arch)"
 
 apk update
 apk add --no-cache arch-install-scripts
 
-if [ "$(cat /etc/apk/arch)" = "armv7" ]; then
+if [ "${ARCHITECTURE}" = "${ARMV7_ARCHITECTURE}" ]; then
   sed -i '/Architecture/s/auto/armv7h/' /etc/pacman.conf
 fi
 
@@ -27,3 +30,8 @@ pacman-key --lsign-key "${GPG_KEY_ID}"
 mkdir -p "${MOUNTPOINT}"
 pacstrap "${MOUNTPOINT}" base arch-install-scripts $@
 rm -r "${MOUNTPOINT}/var/cache/pacman/pkg"
+
+if [ "${ARCHITECTURE}" = "${ARMV7_ARCHITECTURE}" ]; then
+  # fix bug in armv7h pacman https://archlinuxarm.org/forum/viewtopic.php?f=57&t=16830
+  sed -i '/^CFLAGS=/,/^[[:upper:]]+=/{s/[[:space:]]*-mno-omit-leaf-frame-pointer\b//g}' "${MOUNTPOINT}/etc/makepkg.conf"
+fi
